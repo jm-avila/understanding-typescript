ce7a8907c5fb427b7739f788cdc06889680d821f
jm-avila <jm.avila1309@icloud.com>
2020-03-03 08:22:42 +0100

    15 & 16: object types

--- section_02/app.ts
+++ section_02/app.ts
@@ -1,13 +1,14 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const additionResult = n1 + n2;
-  if (showResult) console.log(phrase + additionResult);
+const person: { name: string; age: number } = {
+  name: "Ed",
+  age: 99
+};
 
-  return additionResult;
-}
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "The result is ";
-
-add(number1, number2, printResult, resultPhrase);
+const nestedObject: {
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  };
+} = { id: "", price: 0, tags: [""], details: { title: "", description: "" } };
