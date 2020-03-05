4429c495938888e4d110d28d2ba8bb549af0e474
jm-avila <jm.avila1309@icloud.com>
2020-03-05 18:36:47 +0100

    25: Return Types

---
+++ section_02/returnTypes.ts
@@ -0,0 +1,11 @@
+function noReturnIsVoid(value): void {
+  console.log(value);
+}
+
+function numberTimesTwo(num): number {
+  return num * 2;
+}
+
+function upperCase(str): string {
+  return str.upperCase();
+}
