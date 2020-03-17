a33b874168776625eb3cb43e15f39d3fbabde773
jm-avila <jm.avila1309@icloud.com>
2020-03-17 16:41:38 +0100

    Section 05, Classes

--- section_05/src/app.ts
+++ section_05/src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+class Department {
+  // private properties can't be accesed from the outside of the object
+  private employees: string[];
+
+  constructor(private readonly id: string, public name: string) {
+    this.employees = [];
+  }
+
+  describe(this: Department) {
+    console.log(`My name is ${this.name} and my id is ${this.id}`);
+  }
+
+  addEmployee(employeeName: string) {
+    this.employees.push(employeeName);
+  }
+
+  printEmployees(this: Department) {
+    console.log(this.employees);
+  }
+}
+
+const myDepartment = new Department("z343", "Home");
+myDepartment.describe();
+myDepartment.addEmployee("CHM");
+myDepartment.printEmployees();
