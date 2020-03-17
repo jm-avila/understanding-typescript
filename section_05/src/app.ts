287f769fb2622455fe0b54b90bbebd3ce4b78d50
Jose Avila <jm.avila1309@icloud.com>
2020-03-17 18:01:53 +0100

    Section 5, Classes

--- section_05/src/app.ts
+++ section_05/src/app.ts
@@ -1,14 +1,18 @@
-class Department {
+abstract class Department {
   // private properties can't be accesed from the outside of the object
-  private employees: string[];
 
-  constructor(private readonly id: string, public name: string) {
+  static fiscalYear: string = `${new Date().getFullYear()}`;
+  protected employees: string[];
+
+  constructor(protected readonly id: string, public name: string) {
     this.employees = [];
   }
 
-  describe(this: Department) {
-    console.log(`My name is ${this.name} and my id is ${this.id}`);
+  static createEmployee(name: string) {
+    return { name };
   }
+
+  abstract describe(this: Department): void;
 
   addEmployee(employeeName: string) {
     this.employees.push(employeeName);
@@ -19,7 +23,78 @@ class Department {
   }
 }
 
-const myDepartment = new Department("z343", "Home");
-myDepartment.describe();
-myDepartment.addEmployee("CHM");
-myDepartment.printEmployees();
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "Tech");
+  }
+
+  describe() {
+    console.log(`The ${this.name} department has ID: ${this.id}`);
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  private static instance: AccountingDepartment;
+
+  static getInstance(id: string, reports: string[]) {
+    if (this.instance) return this.instance;
+    this.instance = new AccountingDepartment(id, reports);
+    return this.instance;
+  }
+
+  get mostRecentReport() {
+    return this.lastReport;
+  }
+
+  set mostRecentReport(text: string) {
+    this.addReport(text);
+  }
+
+  private constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0] || "";
+  }
+
+  describe() {
+    const lastReport: string = this.mostRecentReport
+      ? `the last report is ${this.mostRecentReport}`
+      : "and have no reports.";
+    console.log(
+      `The ${this.name} department has ID: ${this.id}, ${lastReport}`
+    );
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") return;
+    this.employees.push(name);
+  }
+
+  addReport(report: string) {
+    this.reports.push(report);
+    this.lastReport = report;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const techTeam = new ITDepartment("d1", []);
+techTeam.name = "Tech Team";
+techTeam.describe();
+techTeam.addEmployee("Cho");
+techTeam.printEmployees();
+
+const accounting = AccountingDepartment.getInstance("d1", []);
+accounting.describe();
+accounting.addEmployee("Mario");
+accounting.printEmployees();
+accounting.addReport("Corona is comming");
+accounting.addReport("Corona is here");
+accounting.mostRecentReport = "Corona, Corona, wash your hands";
+accounting.printReports();
+accounting.describe();
+console.log(accounting.mostRecentReport);
+
+console.log("fiscalYear", Department.fiscalYear);
